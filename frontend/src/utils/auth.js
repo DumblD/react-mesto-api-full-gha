a293966a93e08054f1a829ac8fc7eb5f12d29c8a
Artyom Zinin <NicYKN@yandex.ru>
2023-07-10 04:37:38 +0300

    fix: / -> api.domain... when send requests to server

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://mesto-project.nomoreparties.sbs';
+export const BASE_URL = 'https://api.mesto-project.nomoreparties.sbs';
 
 function checkServerResponseState(res) {
   if (res.ok) {
